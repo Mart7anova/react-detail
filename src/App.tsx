ce22c8e3f878e96e0fc26d7090f95ef35569f845
Mart7anova <mart7anova7@gmail.com>
2022-04-18 12:49:22 +0300

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
+function hello() {
+    debugger
+    alert('hello))))')
+}
+
+//hello();
+
+function App() {
+    console.log("App rendering")
+    return (
+        <div>
+            <PageTitle title={"This is App"}/>
+            <PageTitle title={"User"}/>
+            Article 1
+            <Rating value={4}/>
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Music"} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+type PageTitlePropsType={
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("AppTitle rendering")
+    return <h1> {props.title} </h1>
+}
+
+
+export default App;
