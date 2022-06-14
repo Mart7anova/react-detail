f991868bdf5676fb1993a1344ada83d5578bed7e
Mart7anova <mart7anova7@gmail.com>
2022-06-14 13:09:09 +0300

    add UseMemo.stories.tsx

---
+++ src/MyStories/UseMemo.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: 'Use Memo '
+}
+
+
+export const DifficultCounting = () => {
+    const [a, setA] = useState(0)
+    const [b, setB] = useState(0)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let temp = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 1000000) {
+                fake++
+                let c = Math.random()
+            }
+            temp *= i
+        }
+        return temp
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return <>
+        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>Result for a: {resultA}</div>
+        <div>Result for b: {resultB}</div>
+    </>
+}
